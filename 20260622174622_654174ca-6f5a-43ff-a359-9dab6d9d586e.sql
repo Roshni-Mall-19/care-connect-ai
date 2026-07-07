233f3b24f160208f8780356a2856447bfec93f4e
Roshni-Mall-19 <mca25.mall.roshni@gnims.com>
2026-07-07 17:15:06 +0530

    Delete 20260622174622_654174ca-6f5a-43ff-a359-9dab6d9d586e.sql

--- 20260622174622_654174ca-6f5a-43ff-a359-9dab6d9d586e.sql
+++
@@ -1,5 +0,0 @@
-
-ALTER TABLE public.appointments DROP CONSTRAINT IF EXISTS appointments_patient_id_fkey;
-ALTER TABLE public.appointments
-  ADD CONSTRAINT appointments_patient_id_fkey
-  FOREIGN KEY (patient_id) REFERENCES public.profiles(id) ON DELETE CASCADE;
